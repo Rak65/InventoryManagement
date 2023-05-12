224ce42f4cadc7ef93133a249972c1eeeb26a1bd
Rak65 <rakeshp6505@gmail.com>
2023-05-12 22:56:18 +0530

    JSON Inventory Data Management of Rice, Pulses and Wheats

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace InventoryManagement
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string path = "H:\\C# Project\\InventoryManagement\\InventoryManagement\\Inventory.json";
+            ReadTheData readTheData = new ReadTheData();
+
+            InventoryDetails data = readTheData.Read(path);
+
+            for (int i = 0; i < data.typesOfRice.Count; i++)
+            {
+
+                Console.WriteLine(data.typesOfRice[i].name);
+                Console.WriteLine(data.typesOfRice[i].weight);
+                Console.WriteLine(data.typesOfRice[i].price);
+            }
+
+            foreach (var inventory in data.typesOfRice)
+            {
+                Console.WriteLine(inventory.name);
+            }
+        }
+    }
+}
+
---
+++ InventoryManagement/ReadTheData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+    public class ReadTheData
+    {
+        public InventoryDetails Read(string path)
+        {
+
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
