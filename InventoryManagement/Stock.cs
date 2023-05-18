2b0c3427570c18c1594e836aa0bb2f82be9a671d
Rak65 <rakeshp6505@gmail.com>
2023-05-18 22:33:09 +0530

    Print the Stock Report.

---
+++ InventoryManagement/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public int NumOfShares { get; set; }
+        public double SharePrice { get; set; }
+        public double Calculate()
+        {
+            return NumOfShares * SharePrice;
+        }
+    }
+}
---
+++ InventoryManagement/StockPortfolio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class StockPortfolio
+    {
+        public List<Stock> stocks;
+        public StockPortfolio()
+        {
+            stocks = new List<Stock>();
+        }
+        public void AddStock(Stock stock)
+        {
+            stocks.Add(stock);
+        }
+        public double CalculateTotaValue()
+        {
+            double totalVal = 0;
+            foreach (var stock in stocks)
+            {
+                totalVal += stock.Calculate();
+            }
+            return totalVal;
+        }
+        public void StockReport()
+        {
+            Console.WriteLine("\nStock Report");
+
+            foreach (var stock in stocks)
+            {
+                double stockValue = stock.Calculate();
+                Console.WriteLine("Stock Name: " + stock.Name);
+                Console.WriteLine("Number of shares: " + stock.NumOfShares);
+                Console.WriteLine("Share Price: " + stock.SharePrice);
+                Console.WriteLine("Total value: " + stockValue);
+            }
+            double totalValue = CalculateTotaValue();
+            Console.WriteLine("Total Portfolio value: " + totalValue);
+        }
+    }
+}
